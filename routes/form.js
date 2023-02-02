b31f4993e0695c17518f3635c0a615ff7894de01
EngKelvinmugo <kelvinmugo099@gmail.com>
2023-02-02 21:04:56 +0300

    Cleared

---
+++ routes/form.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+
+
+const Firstname = require("../models/firstname");
+const Middlename = require("../models/middlename");
+const Lastname = require("../models/lastname");
+const Email = require("../models/email");
+const Firstname = require("../models/comments");
+const Phonenumber = require("../models/phonenumber");
+const Occupation = require("../models/occupation");
+const Gender = require("../models/gender");
+
+
+module.exports = router;
